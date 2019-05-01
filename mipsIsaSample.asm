bfc43e62f53079d16598207b0664a260ec3a0b3b
Gurkamal Singh <gurkamalpsc@gmail.com>
2019-04-30 19:36:20 -0700

    created two variables that will be holding ints

---
+++ mipsIsaSample.asm
@@ -0,0 +1,7 @@
+# A Sample Program to try out MIPS ISA Instructions
+
+################################################################################
+        .data
+myId:   .word 36        # storing int 36 as a 32 bitword in the variable myId
+nLet:   .word 5         # Storing int 5 as a 32 bit word in variable nLet
+
